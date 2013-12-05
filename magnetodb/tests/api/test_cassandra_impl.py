a4b448f0d01c46afc803498dd5ee28928f665b92
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2013-12-05 17:43:43 +0200

    Cassandra backend: delete table, list table, create table

---
+++ magnetodb/tests/api/test_cassandra_impl.py
@@ -0,0 +1,75 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import os
+import unittest
+
+from magnetodb.common import PROJECT_ROOT_DIR
+from magnetodb.common import config
+from magnetodb.storage.impl import cassandra_impl as impl
+from magnetodb.storage.models import AttributeDefinition
+from magnetodb.storage.models import TableSchema
+
+CONFIG_FILE = os.path.join(PROJECT_ROOT_DIR,
+                           'etc/magnetodb-test.conf')
+
+
+class FakeContext(object):
+    def __init__(self, tenant):
+        self.tenant = tenant
+
+
+class TestCassandraImpl(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCassandraImpl, cls).setUpClass()
+        cls.orig_session = impl.SESSION
+        cls.orig_conf = config.CONF
+        config.parse_args([], default_config_files=[CONFIG_FILE])
+        storage_param = json.loads(config.CONF.storage_param)
+        cluster = impl.cluster.Cluster(**storage_param)
+        impl.SESSION = cluster.connect()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestCassandraImpl, cls).tearDownClass()
+        impl.SESSION = cls.orig_session
+        config.CONF = cls.orig_conf
+
+    def setUp(self):
+        self.context = FakeContext('default_tenant')
+
+    def test_crud_table(self):
+        attrs = [
+            AttributeDefinition('id', 'N'),
+            AttributeDefinition('range', 'S'),
+            AttributeDefinition('indexed', 'S'),
+        ]
+
+        schema = TableSchema('test', attrs, ['id', 'range'], ['indexed'])
+
+        impl.create_table(self.context, schema)
+
+        listed = impl.list_tables(self.context)
+        self.assertEqual(['test'], listed)
+
+        impl.delete_table(self.context, 'test')
+
+        listed = impl.list_tables(self.context)
+        self.assertEqual([], listed)
