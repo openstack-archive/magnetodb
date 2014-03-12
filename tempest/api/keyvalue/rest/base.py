30df1361c58fcf924208d4440a4cbfa2057b462c
Andrei V. Ostapenko <aostapenko@mirantis.com>
2014-03-12 18:40:03 +0200

    Adds MagnetoDB JSON client for tempest
    Adds rest_client, http modules for magnetodb client.
    Adds base class for rest api tests.
    
    Partially implements: bp tempest-rest-api-tests
    Change-Id: I37bd04efccc0829c51738f2f9cfa78c3d875d2c3

--- tempest/api/keyvalue/rest/base.py
+++ tempest/api/keyvalue/rest/base.py
@@ -19,8 +19,8 @@
 
 import tempest.clients
 import tempest.config
+from tempest import exceptions
 import tempest.test
-from tempest.api.keyvalue import test
 from tempest.common.utils import data_utils
 from tempest.openstack.common import log as logging
 
@@ -28,61 +28,99 @@ from tempest.openstack.common import log as logging
 LOG = logging.getLogger(__name__)
 
 
-class MagnetoDBTestCase(test.BotoTestCase):
+class MagnetoDBTestCase(tempest.test.BaseTestCase):
 
     @classmethod
     def setUpClass(cls):
         super(MagnetoDBTestCase, cls).setUpClass()
         cls.os = tempest.clients.Manager()
-        cls.client = cls.os.dynamodb_client
-        cls.errors = cls.dynamodb_error_code
+        cls.client = cls.os.magnetodb_client
+        cls._resource_trash_bin = {}
 
         # SMOKE TABLE: THREADS
         cls.hashkey = 'forum'
         cls.rangekey = 'subject'
 
         cls.smoke_attrs = [
-            {'AttributeName': cls.hashkey, 'AttributeType': 'S'},
-            {'AttributeName': cls.rangekey, 'AttributeType': 'S'}
+            {'attribute_name': cls.hashkey, 'attribute_type': 'S'},
+            {'attribute_name': cls.rangekey, 'attribute_type': 'S'}
         ]
         # add this attribute to smoke_attrs if want to create index
         # TODO(yyekovenko): full list of attrs should be clarified
         cls.index_attrs = [
-            {'AttributeName': 'last_posted_by', 'AttributeType': 'S'},
-            #{'AttributeName': 'message', 'AttributeType': 'S'},
-            #{'AttributeName': 'replies', 'AttributeType': 'N'}
+            {'attribute_name': 'last_posted_by', 'attribute_type': 'S'},
+            #{'attribute_name': 'message', 'attribute_type': 'S'},
+            #{'attribute_name': 'replies', 'attribute_type': 'N'}
         ]
 
         cls.smoke_schema = [
-            {'AttributeName': cls.hashkey, 'KeyType': 'HASH'},
-            {'AttributeName': cls.rangekey, 'KeyType': 'RANGE'}
+            {'attribute_name': cls.hashkey, 'key_type': 'HASH'},
+            {'attribute_name': cls.rangekey, 'key_type': 'RANGE'}
         ]
         cls.smoke_gsi = None
         cls.smoke_lsi = [
             {
-                'IndexName': 'last_posted_by_index',
-                'KeySchema': [
-                    {'AttributeName': cls.hashkey, 'KeyType': 'HASH'},
-                    {'AttributeName': 'last_posted_by', 'KeyType': 'RANGE'}
+                'index_name': 'last_posted_by_index',
+                'key_schema': [
+                    {'attribute_name': cls.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'last_posted_by', 'key_type': 'RANGE'}
                 ],
-                'Projection': {'ProjectionType': 'ALL'}
+                'projection': {'projection_type': 'ALL'}
             }
         ]
-        cls.smoke_throughput = {'ReadCapacityUnits': 1,
-                                'WriteCapacityUnits': 1}
+
+    @classmethod
+    def tearDownClass(cls):
+        """Calls the callables added by addResourceCleanUp,
+        when you overwire this function dont't forget to call this too.
+        """
+        fail_count = 0
+        trash_keys = sorted(cls._resource_trash_bin, reverse=True)
+        for key in trash_keys:
+            (function, pos_args, kw_args) = cls._resource_trash_bin[key]
+            try:
+                LOG.debug("Cleaning up: %s" %
+                          friendly_function_call_str(function, *pos_args,
+                                                     **kw_args))
+                function(*pos_args, **kw_args)
+            except BaseException as exc:
+                fail_count += 1
+                LOG.exception(exc)
+            finally:
+                del cls._resource_trash_bin[key]
+        super(MagnetoDBTestCase, cls).tearDownClass()
+        # NOTE(afazekas): let the super called even on exceptions
+        # The real exceptions already logged, if the super throws another,
+        # does not causes hidden issues
+        if fail_count:
+            raise exceptions.TearDownException(num=fail_count)
+
+    @classmethod
+    def addResourceCleanUp(cls, function, *args, **kwargs):
+        """Adds CleanUp callable, used by tearDownClass.
+        Recommended to a use (deep)copy on the mutable args.
+        """
+        cls._sequence = cls._sequence + 1
+        cls._resource_trash_bin[cls._sequence] = (function, args, kwargs)
+        return cls._sequence
+
+    @classmethod
+    def cancelResourceCleanUp(cls, key):
+        """Cancel Clean up request."""
+        del cls._resource_trash_bin[key]
 
     @classmethod
     def wait_for_table_active(cls, table_name, timeout=120, interval=3):
         def check():
             resp = cls.client.describe_table(table_name)
-            if "Table" in resp and "TableStatus" in resp["Table"]:
-                return resp["Table"]["TableStatus"] == "ACTIVE"
+            if "table" in resp and "table_status" in resp["table"]:
+                return resp["table"]["table_status"] == "ACTIVE"
 
         return tempest.test.call_until_true(check, timeout, interval)
 
     def wait_for_table_deleted(self, table_name, timeout=120, interval=3):
         def check():
-            return table_name not in self.client.list_tables()['TableNames']
+            return table_name not in self.client.list_tables()['table_names']
 
         return tempest.test.call_until_true(check, timeout, interval)
 
@@ -128,3 +166,22 @@ class MagnetoDBTestCase(test.BotoTestCase):
                 )
                 new_items.append(item)
         return new_items
+
+
+def friendly_function_name_simple(call_able):
+    name = ""
+    if hasattr(call_able, "im_class"):
+        name += call_able.im_class.__name__ + "."
+    name += call_able.__name__
+    return name
+
+
+def friendly_function_call_str(call_able, *args, **kwargs):
+    string = friendly_function_name_simple(call_able)
+    string += "(" + ", ".join(map(str, args))
+    if len(kwargs):
+        if len(args):
+            string += ", "
+    string += ", ".join("=".join(map(str, (key, value)))
+              for (key, value) in kwargs.items())
+    return string + ")"
