cb0586d4bdf66dce92d15a2a6fd986ec153b07a2
Alexander Chudnovets <achudnovets@mirantis.com>
2014-03-20 20:42:21 +0200

    Add dummy version of batch_write to cassandra impl
    This is very basic realization of batch_write operation.
    
    Change-Id: I642382b3d4bc91b94336ff5c19c175ef242b4f11
    Implements: blueprint cassandra-batch-operations

---
+++ magnetodb/tests/unittests/storage/impl/test_cassandra_impl.py
@@ -0,0 +1,67 @@
+# Copyright 2014 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import unittest
+
+from magnetodb.storage import models
+from magnetodb.storage.impl import cassandra_impl
+
+
+class CassandraImplTestCase(unittest.TestCase):
+    """The test for Cassandra storage implementation."""
+
+    @mock.patch('magnetodb.storage.impl.cassandra_impl.'
+                'CassandraStorageImpl.delete_item')
+    @mock.patch('magnetodb.storage.impl.cassandra_impl.'
+                'CassandraStorageImpl.put_item')
+    @mock.patch('magnetodb.common.cassandra.cluster.Cluster.connect')
+    def test_execute_write_batch(self, mock_connect, mock_put_item,
+                                 mock_delete_item):
+        conn = cassandra_impl.CassandraStorageImpl()
+        context = mock.Mock(tenant='fake_tenant')
+
+        table_name = 'fake_table'
+
+        request_list = [
+            models.PutItemRequest(table_name, {
+                'id': models.AttributeValue(
+                    models.ATTRIBUTE_TYPE_NUMBER, 1),
+                'range': models.AttributeValue(
+                    models.ATTRIBUTE_TYPE_STRING, '1'),
+                'str': models.AttributeValue(
+                    models.ATTRIBUTE_TYPE_STRING, 'str1'), }),
+            models.PutItemRequest(table_name, {
+                'id': models.AttributeValue(
+                    models.ATTRIBUTE_TYPE_NUMBER, 2),
+                'range': models.AttributeValue(
+                    models.ATTRIBUTE_TYPE_STRING, '1'),
+                'str': models.AttributeValue(
+                    models.ATTRIBUTE_TYPE_STRING, 'str1'), }),
+            models.DeleteItemRequest(table_name, {
+                'id': models.AttributeValue.number(3),
+                'range': models.AttributeValue.str('3')})
+        ]
+
+        expected_put = [mock.call(context, request_list[0]),
+                        mock.call(context, request_list[1]), ]
+        excpected_delete = [mock.call(context, request_list[2])]
+
+        result = conn.execute_write_batch(context, request_list)
+
+        self.assertEqual(expected_put, mock_put_item.call_args_list)
+        self.assertEqual(excpected_delete, mock_delete_item.call_args_list)
+
+        self.assertEqual({}, result)
