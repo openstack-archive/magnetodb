4f08c98cc1f0377e81fc037ae15a59073564e035
Aleksey Chuprin <achuprin@mirantis.com>
2014-03-24 15:01:04 +0200

    Fixed adding of address to dev lo,resolved some requirements Add function for rejoin-stack.sh, clean.sh
    Change-Id: Ie53854094b80dfd16c6fbdd9c2bd2ebf0e4e2084

--- contrib/devstack/extras.d/70-magnetodb.sh
+++ contrib/devstack/extras.d/70-magnetodb.sh
@@ -1,29 +1,45 @@
 if is_service_enabled magnetodb; then
+
+	if is_service_enabled magnetodb; then
+	MAGNETODB_BACKEND=cassandra
+	fi
+
     if [[ "$1" == "source" ]]; then
         # Initial source
         source $TOP_DIR/lib/magnetodb
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         echo "Installing Magnetodb"
+		install_${MAGNETODB_BACKEND}
         install_magnetodb
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
 	echo "Configuring Magneto"
+
+		configure_${MAGNETODB_BACKEND}
+
 	configure_magnetodb        
 
 
 :
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # no-op
+
+		start_${MAGNETODB_BACKEND}
+		#waiting for backend start
         start_magnetodb
+
+
     fi
 
     if [[ "$1" == "unstack" ]]; then
 	echo  "Stopping Magnetodb"
+
+		stop_${MAGNETODB_BACKEND}
         stop_magnetodb
     fi
 
     if [[ "$1" == "clean" ]]; then
-        # no-op
         :
+		clean_${MAGNETODB_BACKEND}
     fi
 fi
 
