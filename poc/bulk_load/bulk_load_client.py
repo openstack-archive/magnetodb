6b309248268d4cb893197a0d99a8628e85709035
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2014-04-02 14:36:33 -0700

    Bulk data load
    Implemented server for data uploading
    via streaming HTTP POST request
    
    Implements blueprint: ingest-from-big-data-analytics
    
    Change-Id: I6b92110c5751986705ce116d979b3064407994ee

---
+++ poc/bulk_load/bulk_load_client.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import base64
+import json
+import sys
+
+import requests
+
+
+def read_table_structure(filename):
+    structure = []
+    with open(filename, 'r') as f:
+        for line in f:
+            field, typ = line.strip().split()
+            structure.append((field, typ))
+
+    return structure
+
+
+def decode_value(typ, value):
+    if typ == 'S':
+        return value
+    elif typ == 'B':
+        return base64.encodestring(value).strip()
+    elif typ == 'N':
+        return value if value else '0'
+    elif typ in ('NS', 'SS', 'BS'):
+        vals = value[1:-1].split(',')
+
+        single_type = typ[0]
+
+        if single_type == 'N':
+            return [v.strip() if v.strip() else '0'
+                    for v in vals]
+        elif single_type == 'B':
+            return [base64.encodestring(v.strip()).strip()
+                    for v in vals]
+        else:
+            return [v.strip()
+                    for v in vals]
+
+        assert False
+
+
+def make_json(structure, item):
+    tokens = item.strip().split('\x01')
+
+    assert len(structure) == len(tokens)
+
+    res = {
+        field: {typ: decode_value(typ, value.strip())}
+        for (field, typ), value
+        in zip(structure, tokens)
+        if value.strip()
+    }
+
+    return res
+
+
+def get_items(table_def_filename, data_filename):
+    table_str = read_table_structure(table_def_filename)
+
+    with open(data_filename, 'r') as f:
+        for line in f:
+            yield json.dumps(make_json(table_str, line)) + '\n'
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print 'python {} <table_def.file> <data.file>'.format(sys.argv[0])
+
+    table_def_filename, data_filename = sys.argv[1:]
+
+    url = ('http://localhost:8488' +
+           '/v1/default_tenant/data/tables/bigdata/bulk_load')
+
+    r = requests.post(
+        url,
+        data=get_items(table_def_filename, data_filename),
+        headers={'Content-Type': 'application/json'})
+
+    print r.content
