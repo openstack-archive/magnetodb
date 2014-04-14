2851d2238bdf577604f744a356724b5bbf49b9d1
Aleksey Chuprin <aleksei_chuprin@symantec.com>
2014-04-14 10:17:47 +0300

    Added new artifacts for tempest in html format
     - tempest-stabel.html
     - tempest-in-progress.html
     - tempest-not-ready.html
    
    Change-Id: Ic5ba5eb337c03e5735475f961971fd2a806669dc
    Implements: blueprint add-tempest-artifact

--- functionaltests/run_tests.sh
+++ functionaltests/run_tests.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 #This script will run tepmest
 TEMPEST_DIR=${TEMPEST_DIR:-/opt/stack/new/magnetodb/tempest}
+LOGS_DIR=/opt/stack/logs
 cd $TEMPEST_DIR
 
 echo '============== Start stable tests ==============='
-nosetests -v api/keyvalue/stable/
-RETVAL=$?
+stdbuf -oL -eL nosetests --with-subunit api/keyvalue/stable/ | tee $LOGS_DIR/tempest-stable.txt
+RETVAL=${PIPESTATUS[0]}
 echo '============ Start in_progress tests ============'
-nosetests -v api/keyvalue/in_progress/ || true
+sudo nosetests --with-subunit api/keyvalue/in_progress/ | tee $LOGS_DIR/tempest-in-progress.txt
 echo '============ Start not_ready tests =============='
-nosetests -v api/keyvalue/not_ready/ || true
+nosetests --with-subunit api/keyvalue/not_ready/ | tee $LOGS_DIR/tempest-not-ready.txt
 exit $RETVAL
