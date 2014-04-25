a0b7d182ea0edef303aae8f98d0882d0012ced47
dukhlov <dukhlov@mirantis.com>
2014-04-25 13:16:13 -0700

    Storage refactoring for async table creation pluggability
    Storage was separated for 2 layers:
     - StorageManager (responsible for performing operation logic - async or not, etc)
     - StorageDriver (is adapter for target storage with provide common interface)
    
    Also TableInfoRepository entity was separated. It is responsible for storing and loading table meta information like schema
    
    Implements: blueprint async-table-creation
    
    Change-Id: Ieb5057565173b02b1c80742c906ef1ccf423809e

---
+++ magnetodb/storage/driver/cassandra/__init__.py
@@ -0,0 +1,13 @@
+SYSTEM_COLUMN_INDEX_NAME = 'index_name'
+SYSTEM_COLUMN_INDEX_VALUE_STRING = 'index_value_string'
+SYSTEM_COLUMN_INDEX_VALUE_NUMBER = 'index_value_number'
+SYSTEM_COLUMN_INDEX_VALUE_BLOB = 'index_value_blob'
+
+SYSTEM_COLUMN_EXTRA_ATTR_DATA = 'extra_attr_data'
+SYSTEM_COLUMN_EXTRA_ATTR_TYPES = 'extra_attr_types'
+SYSTEM_COLUMN_ATTR_EXIST = 'attr_exist'
+
+USER_PREFIX = 'user_'
+
+
+
