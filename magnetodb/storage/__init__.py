d484c67aaa039ee75c9f787d393c324aece62ea4
dukhlov <dukhlov@mirantis.com>
2014-04-25 13:16:13 -0700

    Storage refactoring for async table creation pluggability
    Storage was separated for 2 layers:
     - StorageManager (responsible for performing operation logic - async or not, etc)
     - StorageDriver (is adapter for target storage with provide common interface)
    
    Also TableInfoRepository entity was separated. It is responsible for storing and loading table meta information like schema
    
    Implements: blueprint async-table-creation
    
    Change-Id: Ieb5057565173b02b1c80742c906ef1ccf423809e

--- magnetodb/storage/__init__.py
+++ magnetodb/storage/__init__.py
@@ -12,24 +12,61 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import ujson as json
 
 from magnetodb.common import config
+
 from magnetodb.openstack.common import importutils
 
-from magnetodb.openstack.common import jsonutils
+from magnetodb.openstack.common import log as logging
 
-__STORAGE_IMPL = None
+LOG = logging.getLogger(__name__)
+
+__STORAGE_MANAGER_IMPL = None
 
 
 def setup():
-    global __STORAGE_IMPL
-    assert __STORAGE_IMPL is None
+    global __STORAGE_MANAGER_IMPL
+    assert __STORAGE_MANAGER_IMPL is None
 
-    storage_param = jsonutils.loads(config.CONF.storage_param)
+    storage_manager_config = config.CONF.storage_manager_config
 
-    __STORAGE_IMPL = importutils.import_class(config.CONF.storage_impl)(
-        **storage_param
-    )
+    object_spec_list = json.loads(storage_manager_config).items()
+
+    context = {}
+
+    while object_spec_list:
+        name, obj_spec = object_spec_list.pop(0)
+        params = obj_spec["params"]
+        res_params = {}
+        for param_name, param_value in params.iteritems():
+            if isinstance(param_value, basestring):
+                if len(param_value) > 1 and param_value[0] == "@":
+                    param_value = param_value[1:]
+                    if param_value[0] != "@":
+                        if param_value in context:
+                            param_value = context[param_value]
+                        elif param_value not in storage_manager_config:
+                            raise AttributeError(
+                                "Object for reference @{} is not found".format(
+                                    param_value
+                                )
+                            )
+                        else:
+                            object_spec_list.append([name, obj_spec])
+                            break
+            res_params[param_name] = param_value
+        else:
+            type_str = obj_spec["type"]
+            mod_str, _sep, class_str = type_str.rpartition('.')
+            if mod_str:
+                module = importutils.import_module(mod_str)
+                cls = getattr(module, class_str)
+            else:
+                cls = eval(type_str)
+            context[name] = cls(**res_params)
+
+    __STORAGE_MANAGER_IMPL = context["storage_manager"]
 
 
 def create_table(context, table_name, table_schema):
@@ -44,7 +81,8 @@ def create_table(context, table_name, table_schema):
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.create_table(context, table_name, table_schema)
+    return __STORAGE_MANAGER_IMPL.create_table(context, table_name,
+                                               table_schema)
 
 
 def delete_table(context, table_name):
@@ -54,9 +92,11 @@ def delete_table(context, table_name):
     @param context: current request context
     @param table_name: String, name of table to delete
 
+    @return TableMeta instance with metadata of created table
+
     @raise BackendInteractionException
     """
-    __STORAGE_IMPL.delete_table(context, table_name)
+    return __STORAGE_MANAGER_IMPL.delete_table(context, table_name)
 
 
 def describe_table(context, table_name):
@@ -66,11 +106,11 @@ def describe_table(context, table_name):
     @param context: current request context
     @param table_name: String, name of table to describes
 
-    @return: TableSchema instance
+    @return: TableMeta instance
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.describe_table(context, table_name)
+    return __STORAGE_MANAGER_IMPL.describe_table(context, table_name)
 
 
 def list_tables(context, exclusive_start_table_name=None, limit=None):
@@ -82,39 +122,64 @@ def list_tables(context, exclusive_start_table_name=None, limit=None):
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.list_tables(context,
-                                      exclusive_start_table_name, limit)
+    return __STORAGE_MANAGER_IMPL.list_tables(
+        context, exclusive_start_table_name, limit
+    )
 
 
 def put_item(context, put_request, if_not_exist=False,
              expected_condition_map=None):
     """
     @param context: current request context
-    @param put_request: contains PutItemRequest items to perform
-                put item operation
-    @param if_not_exist: put item only is row is new record (It is possible to
-                use only one of if_not_exist and expected_condition_map
+    @param put_request: PutItemRequest instance which specifies item to put
+    @param if_not_exist: put item only is row is new record (It is possible
+                to use only one of if_not_exist and expected_condition_map
                 parameter)
     @param expected_condition_map: expected attribute name to
-                ExpectedCondition instance mapping. It provides preconditions
-                to make decision about should item be put or not
+                ExpectedCondition instance mapping. It provides
+                preconditions to make decision about should item be put or
+                not
 
     @return: True if operation performed, otherwise False
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.put_item(context, put_request, if_not_exist,
-                                   expected_condition_map)
+    return __STORAGE_MANAGER_IMPL.put_item(
+        context, put_request, if_not_exist, expected_condition_map
+    )
+
+
+def put_item_async(context, put_request, if_not_exist=False,
+                   expected_condition_map=None):
+    """
+    @param context: current request context
+    @param put_request: PutItemRequest instance which specifies item to put
+    @param if_not_exist: put item only is row is new record (It is possible
+                to use only one of if_not_exist and expected_condition_map
+                parameter)
+    @param expected_condition_map: expected attribute name to
+                ExpectedCondition instance mapping. It provides
+                preconditions to make decision about should item be put or
+                not
+
+    @return: Future instance
+
+    @raise BackendInteractionException
+    """
+    return __STORAGE_MANAGER_IMPL.put_item_async(
+        context, put_request, if_not_exist, expected_condition_map
+    )
 
 
 def delete_item(context, delete_request, expected_condition_map=None):
     """
     @param context: current request context
-    @param delete_request: contains DeleteItemRequest items to perform
-                delete item operation
+    @param delete_request: DeleteItemRequest instance which identifies item
+                to delete
     @param expected_condition_map: expected attribute name to
-                ExpectedCondition instance mapping. It provides preconditions
-                to make decision about should item be deleted or not
+                ExpectedCondition instance mapping. It provides
+                preconditions to make decision about should item be deleted
+                or not
 
     @return: True if operation performed, otherwise False (if operation was
                 skipped by out of date timestamp, it is considered as
@@ -122,61 +187,87 @@ def delete_item(context, delete_request, expected_condition_map=None):
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.delete_item(context, delete_request,
-                                      expected_condition_map)
+    return __STORAGE_MANAGER_IMPL.delete_item(
+        context, delete_request, expected_condition_map
+    )
+
+
+def delete_item_async(context, delete_request,
+                      expected_condition_map=None):
+    """
+    @param context: current request context
+    @param delete_request: DeleteItemRequest instance which identifies item
+                to delete
+    @param expected_condition_map: expected attribute name to
+                ExpectedCondition instance mapping. It provides
+                preconditions to make decision about should item be deleted
+                or not
+
+    @return: Future instance
+
+    @raise BackendInteractionException
+    """
+    return __STORAGE_MANAGER_IMPL.delete_item_async(
+        context, delete_request, expected_condition_map
+    )
 
 
 def execute_write_batch(context, write_request_list):
     """
     @param context: current request context
-    @param write_request_list: contains WriteItemBatchableRequest items to
-                perform batch
+    @param write_request_list: contains PutItemRequest or DeleteItemRequest
+                instances to execute batch operation
 
-    @return: List of unprocessed items
-    @raise BackendInteractionException
+    @return: Unprocessed request list
     """
-    return __STORAGE_IMPL.execute_write_batch(context, write_request_list)
+    return __STORAGE_MANAGER_IMPL.execute_write_batch(context,
+                                                      write_request_list)
 
 
-def update_item(context, table_name, key_attribute_map, attribute_action_map,
-                expected_condition_map=None):
+def update_item(context, table_name, key_attribute_map,
+                attribute_action_map, expected_condition_map=None):
     """
     @param context: current request context
     @param table_name: String, name of table to delete item from
     @param key_attribute_map: key attribute name to
                 AttributeValue mapping. It defines row it to update item
-    @param attribute_action_map: attribute name to UpdateItemAction instance
-                mapping. It defines actions to perform for each given attribute
+    @param attribute_action_map: attribute name to UpdateItemAction
+                instance mapping. It defines actions to perform for each
+                given attribute
     @param expected_condition_map: expected attribute name to
-                ExpectedCondition instance mapping. It provides preconditions
+                ExpectedCondition instance mapping. It provides
+                preconditions
                 to make decision about should item be updated or not
     @return: True if operation performed, otherwise False
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.update_item(context, table_name, key_attribute_map,
-                                      attribute_action_map,
-                                      expected_condition_map)
+    return __STORAGE_MANAGER_IMPL.update_item(
+        context, table_name, key_attribute_map, attribute_action_map,
+        expected_condition_map
+    )
 
 
-def select_item(context, table_name, indexed_condition_map, select_type=None,
-                index_name=None, limit=None, exclusive_start_key=None,
-                consistent=True, order_type=None):
+def select_item(context, table_name, indexed_condition_map=None,
+                select_type=None, index_name=None, limit=None,
+                exclusive_start_key=None, consistent=True,
+                order_type=None):
     """
     @param context: current request context
     @param table_name: String, name of table to get item from
     @param indexed_condition_map: indexed attribute name to
                 IndexedCondition instance mapping. It defines rows
                 set to be selected
-    @param select_type: SelectType instance. It defines with attributes will be
-                returned. If not specified, default will be used:
+    @param select_type: SelectType instance. It defines with attributes
+                will be returned. If not specified, default will be used:
                     SelectType.all() for query on table and
                     SelectType.all_projected() for query on index
     @param index_name: String, name of index to search with
     @param limit: maximum count of returned values
-    @param exclusive_start_key: key attribute names to AttributeValue instance
-    @param consistent: define is operation consistent or not (by default it is
-                not consistent)
+    @param exclusive_start_key: key attribute names to AttributeValue
+                instance
+    @param consistent: define is operation consistent or not (by default it
+                is not consistent)
     @param order_type: defines order of returned rows, if 'None' - default
                 order will be used
 
@@ -184,9 +275,9 @@ def select_item(context, table_name, indexed_condition_map, select_type=None,
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.select_item(
-        context, table_name, indexed_condition_map, select_type,
-        index_name, limit, exclusive_start_key, consistent, order_type
+    return __STORAGE_MANAGER_IMPL.select_item(
+        context, table_name, indexed_condition_map, select_type, index_name,
+        limit, exclusive_start_key, consistent, order_type
     )
 
 
@@ -199,20 +290,19 @@ def scan(context, table_name, condition_map, attributes_to_get=None,
     @param condition_map: attribute name to
                 IndexedCondition instance mapping. It defines rows
                 set to be selected
-    @param attributes_to_get: list of attribute names to be included in result.
-                if None, all attributes will be included
+    @param attributes_to_get: list of attribute names to be included in
+                result. If None, all attributes will be included
     @param limit: maximum count of returned values
-    @param exclusive_start_key: key attribute names to AttributeValue instance
-    @param consistent: define is operation consistent or not (by default it is
-                not consistent)
+    @param exclusive_start_key: key attribute names to AttributeValue
+                instance
+    @param consistent: define is operation consistent or not (by default it
+                is not consistent)
 
     @return list of attribute name to AttributeValue mappings
 
     @raise BackendInteractionException
     """
-    return __STORAGE_IMPL.scan(
-        context, table_name, condition_map,
-        attributes_to_get=attributes_to_get,
-        limit=limit, exclusive_start_key=exclusive_start_key,
-        consistent=consistent
+    return __STORAGE_MANAGER_IMPL.scan(
+        context, table_name, condition_map, attributes_to_get, limit,
+        exclusive_start_key, consistent=False
     )
