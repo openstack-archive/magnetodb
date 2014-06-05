7a371d67a4922c984ab12ee3b596bbe5de11d1be
Charles Wang <charles_wang@symantec.com>
2014-06-04 23:54:26 -0400

    Integrate with Notification System
    Integrate with oslo notifier to send out notifications on table CRUD
    and data item CRUD activities.
    
    Change-Id: I0fc228d15392b6ba58e892b471adf9acd51f178a
    Implements: blueprint magnetodb-notifications

--- magnetodb/storage/manager/async_simple_impl.py
+++ magnetodb/storage/manager/async_simple_impl.py
@@ -12,10 +12,14 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
 import logging
 
 from magnetodb.common.exception import ResourceInUseException
+from magnetodb.common.exception import TableAlreadyExistsException
+from magnetodb.common.exception import TableNotExistsException
+from magnetodb.common.notifier.event import Notification
+from magnetodb.common.notifier.event import notify
+
 from magnetodb.storage import models
 from magnetodb.storage.manager.simple_impl import SimpleStorageManager
 from magnetodb.storage.table_info_repo import TableInfo
@@ -24,10 +28,24 @@ LOG = logging.getLogger(__name__)
 
 
 class AsyncSimpleStorageManager(SimpleStorageManager):
+    def __init__(self, storage_driver,
+                 table_info_repo,
+                 concurrent_tasks=1000):
+        SimpleStorageManager.__init__(self, storage_driver,
+                                      table_info_repo,
+                                      concurrent_tasks)
+
     def create_table(self, context, table_name, table_schema):
+        notify(context, Notification.TABLE_CREATE_START, table_schema)
+
         table_info = TableInfo(table_name, table_schema,
                                models.TableMeta.TABLE_STATUS_CREATING)
-        self._table_info_repo.save(context, table_info)
+        try:
+            self._table_info_repo.save(context, table_info)
+        except TableAlreadyExistsException as e:
+            notify(context, Notification.TABLE_CREATE_ERROR, e.message,
+                   priority=Notification.ERROR)
+            raise
 
         future = self._execute_async(self._storage_driver.create_table,
                                      context=context,
@@ -42,19 +60,37 @@ class AsyncSimpleStorageManager(SimpleStorageManager):
                 self._table_info_repo.update(
                     context, latest_table_info, ["status"]
                 )
+                notify(context, Notification.TABLE_CREATE_END, table_schema)
+            else:
+                notify(context, Notification.TABLE_CREATE_ERROR,
+                       future.exception(),
+                       priority=Notification.ERROR)
 
         future.add_done_callback(callback)
 
         return models.TableMeta(table_info.schema, table_info.status)
 
     def delete_table(self, context, table_name):
-        table_info = self._table_info_repo.get(context, table_name, ['status'])
+        notify(context, Notification.TABLE_DELETE_START, table_name)
+        try:
+            table_info = self._table_info_repo.get(context,
+                                                   table_name,
+                                                   ['status'])
+        except TableNotExistsException as e:
+            notify(context, Notification.TABLE_DELETE_ERROR, e.message,
+                   priority=Notification.ERROR)
+            raise
 
         # table has to be in active status to be deleted
         if table_info.status == models.TableMeta.TABLE_STATUS_CREATING:
-            raise ResourceInUseException()
+            e = ResourceInUseException()
+            notify(context, Notification.TABLE_DELETE_ERROR,
+                   table_name + ' ' + e.message,
+                   priority=Notification.ERROR)
+            raise e
         elif table_info.status == models.TableMeta.TABLE_STATUS_DELETING:
             # table is already being deleted, just return immediately
+            notify(context, Notification.TABLE_DELETE_END, table_name)
             return models.TableMeta(table_info.schema, table_info.status)
 
         table_info.status = models.TableMeta.TABLE_STATUS_DELETING
@@ -70,6 +106,10 @@ class AsyncSimpleStorageManager(SimpleStorageManager):
                 self._table_info_repo.delete(
                     context, table_name
                 )
+                notify(context, Notification.TABLE_DELETE_END, table_name)
+            else:
+                notify(context, Notification.TABLE_DELETE_ERROR,
+                       future.exception(), priority=Notification.ERROR)
 
         future.add_done_callback(callback)
         return models.TableMeta(table_info.schema, table_info.status)
