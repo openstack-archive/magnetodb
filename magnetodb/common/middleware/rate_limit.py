4c0a1b0f3502e397063839a488970504110a4c45
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2014-07-31 17:41:55 +0300

    Rate limit per tenant
    Add rate limiting per tenant per single node,
    or per gunicorn worker in gunicorn enabled configuration
    
    Implements blueprint: rate-limit-per-tenant
    
    Change-Id: Ib8b39e94a74977f9b8174be686d4810c05ad7e0a

---
+++ magnetodb/common/middleware/rate_limit.py
@@ -0,0 +1,66 @@
+# Copyright 2014 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""A middleware that limits amount of processed requests per tenant
+up to configured value
+"""
+
+import re
+import time
+
+from magnetodb.common import exception
+from magnetodb.common import wsgi
+from magnetodb.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class RateLimitMiddleware(wsgi.Middleware):
+    def __init__(self, app, options):
+        self.options = options
+        try:
+            self.rps_per_tenant = int(self.options.get('rps_per_tenant', 0))
+        except Exception as e:
+            LOG.error('Error defining request rate, {}', e)
+            LOG.error('Rate limiting disabled')
+            self.rps_per_tenant = 0
+        super(RateLimitMiddleware, self).__init__(app)
+
+    def process_request(self, req):
+        tenant_id = self._get_tenant_id(req)
+
+        now = time.time()
+        prev = self.last_time.get(tenant_id, 0)
+
+        if self.rps_per_tenant and now - prev < 1. / self.rps_per_tenant:
+            raise exception.RequestQuotaExceeded()
+
+        self.last_time[tenant_id] = now
+
+    @classmethod
+    def factory_method(cls, global_config, **local_config):
+        return lambda application: cls(application, local_config)
+
+    @staticmethod
+    def _get_tenant_id(req):
+        path = req.path
+
+        LOG.debug('Request path: %s', path)
+
+        if not re.match("^/v1/\w+", path):
+            raise exception.ResourceNotFoundException()
+
+        url_comp = path.split('/')
+        return url_comp[2]
+
