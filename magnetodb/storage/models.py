1713b98a4558e6d2c14470dde27e7ef638a1fc66
Dmitriy Ukhlov <dukhlov@mirantis.com>
2014-08-21 19:35:49 +0300

    test
    Change-Id: I23bbf0e03db801c75c426c5291550da2a0e78ae1

--- magnetodb/storage/models.py
+++ magnetodb/storage/models.py
@@ -238,7 +238,7 @@ class AttributeValue(ModelBase):
                 return value
         elif value_type == AttributeType.PRIMITIVE_TYPE_BLOB:
             if isinstance(value, basestring):
-                return base64.b64decode(value)
+                return buffer(base64.b64decode(value))
 
         return None
 
@@ -298,8 +298,7 @@ class AttributeValue(ModelBase):
         elif value_type == AttributeType.PRIMITIVE_TYPE_NUMBER:
             return str(value)
         elif value_type == AttributeType.PRIMITIVE_TYPE_BLOB:
-            if isinstance(value, basestring):
-                return base64.b64encode(value)
+            return base64.b64encode(value)
 
         return None
 
