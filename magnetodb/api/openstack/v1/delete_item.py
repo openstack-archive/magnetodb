c92c106a5d4f6921988cb26dd49bd435d3783697
Dmitriy Ukhlov <dukhlov@mirantis.com>
2014-09-18 18:27:17 +0300

    Improve json schema validation for left requests
    Change-Id: I31eafeabb4330fb9f1c7f146f51401e3c360b79b
    Implements: bp implement-table-schema-based-validation

--- magnetodb/api/openstack/v1/delete_item.py
+++ magnetodb/api/openstack/v1/delete_item.py
@@ -13,90 +13,67 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import jsonschema
-
 from magnetodb import storage
+from magnetodb.api import validation
 from magnetodb.api.openstack.v1 import parser
 from magnetodb.api.openstack.v1 import utils
 from magnetodb.common import probe
+from magnetodb.storage.models import DeleteReturnValuesType
 
 
 class DeleteItemController(object):
     """ Deletes a single item in a table by primary key. """
 
-    schema = {
-        "required": [parser.Props.KEY],
-        "properties": {
-            parser.Props.EXPECTED: {
-                "type": "object",
-                "patternProperties": {
-                    parser.ATTRIBUTE_NAME_PATTERN: {
-                        "oneOf": [
-                            {
-                                "type": "object",
-                                "required": [parser.Props.EXISTS],
-                                "properties": {
-                                    parser.Props.EXISTS: {
-                                        "type": "boolean",
-                                    },
-                                }
-                            },
-                            {
-                                "type": "object",
-                                "required": [parser.Props.VALUE],
-                                "properties": {
-                                    parser.Props.VALUE:
-                                        parser.Types.TYPED_ATTRIBUTE_VALUE,
-                                }
-                            },
-                        ]
-                    }
-                }
-            },
-
-            parser.Props.KEY: parser.Types.ITEM,
-
-            parser.Props.RETURN_VALUES: {
-                "type": "string",
-                "enum": [parser.Values.RETURN_VALUES_NONE,
-                         parser.Values.RETURN_VALUES_ALL_OLD]
-            },
-            parser.Props.TABLE_NAME: parser.Types.TABLE_NAME
-        }
-    }
-
     @probe.Probe(__name__)
     def process_request(self, req, body, project_id, table_name):
-        with probe.Probe(__name__ + '.jsonschema.validate'):
-            jsonschema.validate(body, self.schema)
         utils.check_project_id(req.context, project_id)
+        req.context.tenant = project_id
 
-        # parse expected item conditions
-        expected_item_conditions = (
-            parser.Parser.parse_expected_attribute_conditions(
-                body.get(parser.Props.EXPECTED, {})
+        with probe.Probe(__name__ + '.jsonschema.validate'):
+            validation.validate_object(body, "body")
+
+            # parse expected item conditions
+            expected_item_conditions_json = body.pop(parser.Props.EXPECTED,
+                                                     None)
+            if expected_item_conditions_json:
+                validation.validate_object(expected_item_conditions_json,
+                                           parser.Props.EXPECTED)
+                expected_item_conditions = (
+                    parser.Parser.parse_expected_attribute_conditions(
+                        expected_item_conditions_json
+                    )
+                )
+            else:
+                expected_item_conditions = None
+
+            # parse key_attributes
+            key_attributes_json = body.pop(parser.Props.KEY, None)
+            validation.validate_object(key_attributes_json, parser.Props.KEY)
+
+            key_attributes = parser.Parser.parse_item_attributes(
+                key_attributes_json
             )
-        )
 
-        # parse key_attributes
-        key_attributes = parser.Parser.parse_item_attributes(
-            body.get(parser.Props.KEY, {})
-        )
+            # parse return_values param
+            return_values_json = body.pop(
+                parser.Props.RETURN_VALUES, parser.Values.RETURN_VALUES_NONE
+            )
 
-        # parse return_values param
-        return_values = body.get(
-            parser.Props.RETURN_VALUES, parser.Values.RETURN_VALUES_NONE
-        )
+            validation.validate_string(return_values_json,
+                                       parser.Props.RETURN_VALUES)
+
+            return_values = DeleteReturnValuesType(return_values_json)
+
+            validation.validate_unexpected_props(body, "body")
 
         # delete item
-        req.context.tenant = project_id
         storage.delete_item(req.context, table_name, key_attributes,
                             expected_condition_map=expected_item_conditions)
 
         # format response
         response = {}
 
-        if return_values != parser.Values.RETURN_VALUES_NONE:
+        if return_values.type != parser.Values.RETURN_VALUES_NONE:
             # TODO(cwang):
             # It is needed to return all deleted item attributes
             #
