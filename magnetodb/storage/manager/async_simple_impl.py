8793881fe3fda609eb90a73c9342a5238f6804cd
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2014-09-25 12:10:20 +0300

    Add 'last_updated' to 'table_info'
    Update table status to *_FAILED if it stucks
    to CREATING/DELETING state for a long time
    
    Implements blueprint: table-info-timestamp
    
    Change-Id: I03659858285082e3eca88846bea3c8fb7a3009cf

--- magnetodb/storage/manager/async_simple_impl.py
+++ magnetodb/storage/manager/async_simple_impl.py
@@ -23,11 +23,12 @@ LOG = logging.getLogger(__name__)
 
 
 class AsyncSimpleStorageManager(SimpleStorageManager):
-    def __init__(self, storage_driver,
-                 table_info_repo,
-                 concurrent_tasks=1000, batch_chunk_size=25):
+    def __init__(self, storage_driver, table_info_repo,
+                 concurrent_tasks=1000, batch_chunk_size=25,
+                 schema_operation_timeout=600):
         SimpleStorageManager.__init__(self, storage_driver, table_info_repo,
-                                      concurrent_tasks, batch_chunk_size)
+                                      concurrent_tasks, batch_chunk_size,
+                                      schema_operation_timeout)
 
     def _do_create_table(self, context, table_info):
         future = self._execute_async(self._storage_driver.create_table,
