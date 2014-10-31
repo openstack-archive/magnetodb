9da7234e71c28570aa6fbfa5d4eb83ab67a669b6
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2014-10-31 13:35:32 +0200

    Raise ValidationError if attribute type is missing
    Change-Id: I92c88f6ee50183a6f2c36cf370dd4b15e56a227c
    Closes-Bug: #1386717

--- tempest/api/keyvalue/stable/rest/test_put_item.py
+++ tempest/api/keyvalue/stable/rest/test_put_item.py
@@ -861,7 +861,7 @@ class MagnetoDBPutItemTest(MagnetoDBTestCase):
             self.client.put_item(self.table_name, new_item, expected)
 
         exception_str = str(raises_cm.exception)
-        self.assertIn("u'message': u\"Attribute type 'KK' isn't recognized\"",
+        self.assertIn("u'message': u\"Attribute type 'KK' is not recognized\"",
                       exception_str)
         self.assertIn("u'type': u'ValidationError'", exception_str)
 
