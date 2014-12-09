2d8a089b09c11535cf734f6d9be0b2e83b55e338
Ajaya Agrawal <ajaya.agrawal@ril.com>
2014-12-09 18:04:13 +0530

    Change payload in notification to contain uuid
    The payload is changed to contain table_uuid
    along with table_name. Cases where uuid is
    not available, the field would be an empty string.
    A new field called index.count is also added when
    magnetodb.table.create.end is genarated. This is
    generated to keep a count of indices in Ceilometer.
    Also updated the log messages to contain uuid.
    
    Changed the user field in context to contain
    user_id instead of user_name.
    
    Closes-bug: #1400695
    
    Change-Id: Ia6bfbf929945f922382dd7d6a0965863edd5b8b7

--- magnetodb/common/middleware/context.py
+++ magnetodb/common/middleware/context.py
@@ -49,7 +49,7 @@ class ContextMiddleware(wsgi.Middleware):
         """
         # Use the default empty context, with admin turned on for
         # backwards compatibility
-        user_id = req.headers.get('X-User-Name', None)
+        user_id = req.headers.get('X-User-Id', None)
         tenant_id = req.headers.get('X-Tenant-Id', None)
         req.context = self.make_context(is_admin=True,
                                         user_id=user_id,
