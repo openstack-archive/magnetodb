a850f64a9b60d5ceec9530eec63de58ec921f02e
Andrei V. Ostapenko <Andrey_Ostapenko@symantec.com>
2014-12-18 18:41:13 +0200

    Adds magnetodb plugin for tempest
    Implements bp: remove-tempest
    Change-Id: I50954506de15cfba25f32e5150da05f4275f025f

---
+++ contrib/tempest/tempest/api/keyvalue/in_progress/rest/test_create_table_negative.py
@@ -0,0 +1,385 @@
+# Copyright 2014 Symantec Corporation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+
+from tempest.api.keyvalue.rest_base.base import MagnetoDBTestCase
+from tempest.common.utils.data_utils import rand_name
+from tempest import exceptions
+from tempest import test
+
+
+class MagnetoDBCreateTableNegativeTestCase(MagnetoDBTestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(MagnetoDBCreateTableNegativeTestCase,
+              self).__init__(*args, **kwargs)
+        self.tname = rand_name().replace('-', '')
+
+    @test.attr(type=['CreT-3', 'CreT-101', 'negative'])
+    def test_duplicate_table(self):
+        self._create_test_table(self.smoke_attrs, self.tname,
+                                self.smoke_schema)
+        with self.assertRaises(exceptions.Duplicate):
+            self._create_test_table(self.smoke_attrs, self.tname,
+                                    self.smoke_schema)
+
+    @test.attr(type=['CreT-20_1', 'negative'])
+    def test_create_table_malformed_attrs_02(self):
+        attr_def = [{'attribute_name': 'test'}]
+        key_schema = [{'attribute_name': 'test', 'key_type': 'HASH'}]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(attr_def=attr_def,
+                                    table_name=self.tname,
+                                    schema=key_schema)
+
+    @test.attr(type=['CreT-36_1', 'negative'])
+    def test_create_table_hash_key_set(self):
+        attr_def = [{'attribute_name': 'set', 'attribute_type': 'SS'}]
+        key_schema = [{'attribute_name': 'set', 'key_type': 'HASH'}]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(attr_def=attr_def,
+                                    table_name=self.tname,
+                                    schema=key_schema)
+
+    @test.attr(type=['CreT-36_2', 'negative'])
+    def test_create_table_range_key_set(self):
+        attr_def = [{'attribute_name': 'set', 'attribute_type': 'SS'}]
+        schema = [{'attribute_name': 'set', 'key_type': 'RANGE'}]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(attr_def=self.one_attr + attr_def,
+                                    table_name=self.tname,
+                                    schema=self.schema_hash_only + schema)
+
+    @test.attr(type=['CreT-46', 'negative'])
+    def test_create_table_too_short_name(self):
+        tname = 'qq'
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + self.index_attrs,
+                                    tname,
+                                    self.smoke_schema,
+                                    self.smoke_lsi,
+                                    wait_for_active=True)
+
+    @test.attr(type=['CreT-47', 'negative'])
+    def test_create_table_too_long_name(self):
+        tname = self.tname + 'q' * (256 - len(self.tname))
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + self.index_attrs,
+                                    tname,
+                                    self.smoke_schema,
+                                    self.smoke_lsi)
+
+    @test.attr(type=['CreT-18', 'negative'])
+    def test_create_table_too_long_attr_names(self):
+        attr_def = [
+            {'attribute_name': 'f' * 256, 'attribute_type': 'S'},
+            {'attribute_name': 's' * 256, 'attribute_type': 'S'}
+        ]
+        schema = [
+            {'attribute_name': 'f' * 256, 'key_type': 'HASH'},
+            {'attribute_name': 's' * 256, 'key_type': 'RANGE'}
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(attr_def, self.tname, schema)
+
+    @test.attr(type=['CreT-56', 'negative'])
+    def test_create_table_six_indexes(self):
+        index_attrs = [{'attribute_name': 'attr_name' + str(i),
+                        'attribute_type': 'S'} for i in range(0, 6)]
+        request_lsi = []
+        for attribute in index_attrs:
+            index_attr_name = attribute['attribute_name']
+            request_lsi.append(
+                {
+                    'index_name': index_attr_name + '_index',
+                    'key_schema': [
+                        {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                        {'attribute_name': index_attr_name,
+                         'key_type': 'RANGE'}
+                    ],
+                    'projection': {'projection_type': 'ALL'}
+                }
+            )
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-57', 'negative'])
+    def test_create_table_empty_indexes(self):
+        request_lsi = []
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-63', 'negative'])
+    def test_create_table_index_name_2_char(self):
+        request_lsi = copy.deepcopy(self.smoke_lsi)
+        request_index_name = 'qq'
+        request_lsi[0]['index_name'] = request_index_name
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + self.index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-64', 'negative'])
+    def test_create_table_index_name_256_char(self):
+        request_lsi = copy.deepcopy(self.smoke_lsi)
+        request_index_name = 'q' * 256
+        request_lsi[0]['index_name'] = request_index_name
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + self.index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-65', 'negative'])
+    def test_create_table_index_name_upper_case(self):
+        # TODO(aostapenko) clarify behavior on dynamodb
+        request_lsi = copy.deepcopy(self.smoke_lsi)
+        request_index_name = 'INDEX_NAME'
+        request_lsi[0]['index_name'] = request_index_name
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + self.index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-72', 'negative'])
+    def test_create_table_two_indexes_with_same_key(self):
+        index_attrs = [{'attribute_name': 'attr_name',
+                        'attribute_type': 'S'}]
+        request_lsi = [
+            {
+                'index_name': 'index_name1',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name', 'key_type': 'RANGE'}
+                ],
+                'projection': {'projection_type': 'ALL'}
+            },
+            {
+                'index_name': 'index_name2',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name', 'key_type': 'RANGE'}
+                ],
+                'projection': {'projection_type': 'ALL'}
+            },
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-71', 'negative'])
+    def test_create_table_schema_hash_only_with_index(self):
+        index_attrs = [{'attribute_name': 'attr_name1',
+                        'attribute_type': 'S'},
+                       ]
+        request_lsi = [
+            {
+                'index_name': 'index_name',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name1', 'key_type': 'RANGE'}
+                ],
+                'projection': {'projection_type': 'ALL'}
+            }
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.one_attr + index_attrs,
+                                    self.tname,
+                                    self.schema_hash_only,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-74', 'negative'])
+    def test_create_table_index_schema_repeats_table_schema(self):
+        request_lsi = [
+            {
+                'index_name': 'index_name',
+                'key_schema': self.smoke_schema,
+                'projection': {'projection_type': 'ALL'}
+            }
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-80', 'negative'])
+    def test_create_table_index_without_projection_type_with_non_key(self):
+        index_attrs = [{'attribute_name': 'attr_name1',
+                        'attribute_type': 'S'},
+                       ]
+        request_lsi = [
+            {
+                'index_name': 'index_name',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name1', 'key_type': 'RANGE'}
+                ],
+                'projection': {'non_key_attributes': ['attr1', 'attr2']}
+            }
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-82', 'negative'])
+    def test_create_table_index_key_attr_in_non_key(self):
+        index_attrs = [{'attribute_name': 'attr_name1',
+                        'attribute_type': 'S'},
+                       ]
+        non_key_attributes = ['attr_name1', self.hashkey, self.rangekey]
+        request_lsi = [
+            {
+                'index_name': 'index_name',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name1', 'key_type': 'RANGE'}
+                ],
+                'projection': {
+                    'projection_type': 'INCLUDE',
+                    'non_key_attributes': non_key_attributes}
+            }
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-83', 'negative'])
+    def test_create_table_index_non_key_empty(self):
+        index_attrs = [{'attribute_name': 'attr_name1',
+                        'attribute_type': 'S'},
+                       ]
+        non_key_attributes = []
+        request_lsi = [
+            {
+                'index_name': 'index_name',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name1', 'key_type': 'RANGE'}
+                ],
+                'projection': {
+                    'projection_type': 'INCLUDE',
+                    'non_key_attributes': non_key_attributes}
+            }
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname,
+                                    self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-86', 'negative'])
+    def test_create_table_index_21_non_key(self):
+        index_attrs = [{'attribute_name': 'attr_name1',
+                        'attribute_type': 'S'},
+                       ]
+        non_key_attributes = ['non_key_attr' + i for i in range(0, 21)]
+        request_lsi = [
+            {
+                'index_name': 'index_name',
+                'key_schema': [
+                    {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                    {'attribute_name': 'attr_name1', 'key_type': 'RANGE'}
+                ],
+                'projection': {
+                    'projection_type': 'INCLUDE',
+                    'non_key_attributes': non_key_attributes}
+            }
+        ]
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname, self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-88', 'negative'])
+    def test_create_table_index_21_non_key_3_indexes(self):
+        index_attrs = [{'attribute_name': 'attr_name' + str(i),
+                        'attribute_type': 'S'} for i in range(1, 4)
+                       ]
+        non_key_attributes = (['non_key_attr' + str(i) for i in range(1, 8)],
+                              ['non_key_attr' + str(i) for i in range(8, 15)],
+                              ['non_key_attr' + str(i) for i in range(15, 22)])
+        request_lsi = []
+        for i, attr_name in enumerate(index_attrs):
+            request_lsi.append(
+                {
+                    'index_name': attr_name['attribute_name'] + 'index',
+                    'key_schema': [
+                        {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                        {'attribute_name': attr_name['attribute_name'],
+                         'key_type': 'RANGE'}
+                    ],
+                    'projection': {
+                        'projection_type': 'INCLUDE',
+                        'non_key_attributes': non_key_attributes[i]}
+                }
+            )
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname, self.smoke_schema,
+                                    request_lsi)
+
+    @test.attr(type=['CreT-89', 'negative'])
+    def test_create_table_index_21_non_key_3_indexes_repetitions(self):
+        index_attrs = [{'attribute_name': 'attr_name' + str(i),
+                        'attribute_type': 'S'} for i in range(1, 4)
+                       ]
+        non_key_attributes = (['non_key_attr' + str(i) for i in range(1, 8)],
+                              ['non_key_attr' + str(i) for i in range(5, 12)],
+                              ['non_key_attr' + str(i) for i in range(10, 16)])
+        request_lsi = []
+        for i, attr_name in enumerate(index_attrs):
+            request_lsi.append(
+                {
+                    'index_name': attr_name['attribute_name'] + 'index',
+                    'key_schema': [
+                        {'attribute_name': self.hashkey, 'key_type': 'HASH'},
+                        {'attribute_name': attr_name['attribute_name'],
+                         'key_type': 'RANGE'}
+                    ],
+                    'projection': {
+                        'projection_type': 'INCLUDE',
+                        'non_key_attributes': non_key_attributes[i]}
+                }
+            )
+        with self.assertRaises(exceptions.BadRequest):
+            self._create_test_table(self.smoke_attrs + index_attrs,
+                                    self.tname, self.smoke_schema,
+                                    request_lsi)
+#
+#    @test.attr(type=['CreT-103', 'negative'])
+#    def test_create_table_more_than_255(self):
+#        for i in range(0, 255):
+#            tname = rand_name().replace('-', '')
+#            self._create_test_table(self.smoke_attrs, tname,
+#                                    self.smoke_schema)
+#        with self.assertRaises(exceptions.BadRequest):
+#            self._create_test_table(self.smoke_attrs, self.tname,
+#                                    self.smoke_schema)
---
+++ contrib/tempest/tempest/api/keyvalue/stable/boto/test_tables.py
@@ -0,0 +1,112 @@
+# Copyright 2014 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest.api.keyvalue.boto_base.base import MagnetoDBTestCase
+from tempest.common.utils.data_utils import rand_name
+from tempest.test import attr
+
+
+class MagnetoDBTablesTest(MagnetoDBTestCase):
+
+    def _verify_create_table_response(self, response,
+                                      attributes, table_name,
+                                      key_schema, provisioned_throughput,
+                                      local_secondary_indexes=None,
+                                      global_secondary_indexes=None):
+
+        self.assertTrue('TableDescription' in response)
+        content = response['TableDescription']
+        self.assertEqual(table_name, content['TableName'])
+        self.assertEqual(attributes, content['AttributeDefinitions'])
+        self.assertEqual(0, content['ItemCount'])
+        self.assertEqual(key_schema, content['KeySchema'])
+        self.assertEqual(0, content['TableSizeBytes'])
+
+        expected = provisioned_throughput.copy()
+        expected['NumberOfDecreasesToday'] = 0
+        for key, value in expected.items():
+            self.assertIn(key, content['ProvisionedThroughput'])
+            self.assertIsInstance(value, int)
+
+        if local_secondary_indexes is None:
+            self.assertNotIn('LocalSecondaryIndexes', content)
+        else:
+            expected = local_secondary_indexes.copy()
+            expected['IndexSizeBytes'] = 0
+            expected['IndexStatus'] = 'CREATING'
+            expected['ItemCount'] = 0
+            self.assertEqual(expected, content['LocalSecondaryIndexes'])
+
+        if global_secondary_indexes is None:
+            self.assertNotIn('GlobalSecondaryIndexes', content)
+        else:
+            expected = global_secondary_indexes.copy()
+            # TODO(yyekovenko): Check all attributes of GSI
+            self.assertEqual(expected, content['GlobalSecondaryIndexes'])
+
+        # CreationDateTime
+        # TODO(yyekovenko): 0 returned for now. Find a way to get exp. time
+        self.assertIn('CreationDateTime', content)
+
+        # TODO(yyekovenko): Later should be changed to just "CREATING" (async)
+        self.assertIn(content['TableStatus'], ['CREATING', 'ACTIVE'])
+
+    @attr(type='smoke')
+    def test_create_list_delete_table(self):
+        tname = rand_name().replace('-', '')
+        table = self.client.create_table(self.smoke_attrs + self.index_attrs,
+                                         tname,
+                                         self.smoke_schema,
+                                         self.smoke_throughput)
+        rck = self.addResourceCleanUp(self.client.delete_table, tname)
+        self.assertEqual(dict, type(table))
+        # TODO(yyekovenko) Later should be changed to just "CREATING" (async)
+        self.assertIn(table['TableDescription']['TableStatus'],
+                      ['CREATING', 'ACTIVE'])
+        self.assertEqual(tname, table['TableDescription']['TableName'])
+        self.assertTrue(self.wait_for_table_active(tname))
+
+        tables = self.client.list_tables()
+        self.assertIn(tname, tables['TableNames'])
+
+        descr = self.client.describe_table(tname)
+        self.assertIn('Table', descr)
+        self.assertIn('ItemCount', descr['Table'])
+
+        res = self.client.delete_table(tname)
+        # TODO(yyekovenko) Later should be changed to just "DELETING" (async)
+        self.assertIn(res['TableDescription']['TableStatus'],
+                      ['DELETING', 'ACTIVE'])
+        self.assertTrue(self.wait_for_table_deleted(tname))
+        self.cancelResourceCleanUp(rck)
+
+    def test_create_table_with_required_params_only(self):
+        """
+        Create a table only with required attrs and verify response content
+        """
+        tname = rand_name().replace('-', '')
+        table = self.client.create_table(
+            attribute_definitions=self.smoke_attrs,
+            table_name=tname,
+            key_schema=self.smoke_schema,
+            provisioned_throughput=self.smoke_throughput)
+        self.addResourceCleanUp(self.client.delete_table, tname)
+
+        self._verify_create_table_response(
+            table,
+            self.smoke_attrs, tname, self.smoke_schema,
+            self.smoke_throughput)
+
+        self.assertTrue(self.wait_for_table_active(tname))
