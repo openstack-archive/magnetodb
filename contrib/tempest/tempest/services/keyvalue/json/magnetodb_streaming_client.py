bb648ea814b2c5f6f0329182827d24af8117290c
Andrei V. Ostapenko <Andrey_Ostapenko@symantec.com>
2014-12-18 18:41:13 +0200

    Adds magnetodb plugin for tempest
    Implements bp: remove-tempest
    Change-Id: I50954506de15cfba25f32e5150da05f4275f025f

---
+++ contrib/tempest/tempest/services/keyvalue/json/magnetodb_streaming_client.py
@@ -0,0 +1,41 @@
+# Copyright 2014 Mirantis Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+from tempest.common import rest_client
+from tempest import config_magnetodb as config
+
+CONF = config.CONF
+rest_client.CONF = CONF
+
+
+class MagnetoDBStreamingClientJSON(rest_client.RestClient):
+
+    def __init__(self, *args, **kwargs):
+        super(MagnetoDBStreamingClientJSON, self).__init__(*args, **kwargs)
+        self.service = CONF.magnetodb_streaming.catalog_type
+
+    def upload_items(self, table_name, items):
+        post_body = ''.join([json.dumps(item) + '\n' for item in items])
+        return self.upload_raw_stream(table_name, post_body)
+
+    def upload_raw_stream(self, table_name, stream):
+        url = '/'.join(['tables', table_name, 'bulk_load'])
+        resp, body = self.post(url, stream)
+        return resp, self._parse_resp(body)
+
+    def _parse_resp(self, body):
+        return json.loads(body)
