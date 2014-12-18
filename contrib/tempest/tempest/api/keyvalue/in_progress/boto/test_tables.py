e6e422f4263f81cebca3cbc61b4a494abb2cea09
Andrei V. Ostapenko <Andrey_Ostapenko@symantec.com>
2014-12-18 18:41:13 +0200

    Adds magnetodb plugin for tempest
    Implements bp: remove-tempest
    Change-Id: I50954506de15cfba25f32e5150da05f4275f025f

---
+++ contrib/tempest/tempest/api/keyvalue/in_progress/boto/test_tables.py
@@ -0,0 +1,125 @@
+# Copyright 2014 Mirantis Inc.
+# Copyright 2014 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest.api.keyvalue.boto_base.base import MagnetoDBTestCase
+from tempest.common.utils.data_utils import rand_name
+
+
+class MagnetoDBTablesTest(MagnetoDBTestCase):
+
+    def _verify_create_table_response(self, response,
+                                      attributes, table_name,
+                                      key_schema, provisioned_throughput,
+                                      local_secondary_indexes=None,
+                                      global_secondary_indexes=None):
+
+        self.assertTrue('TableDescription' in response)
+        content = response['TableDescription']
+        self.assertEqual(table_name, content['TableName'])
+        self.assertEqual(attributes, content['AttributeDefinitions'])
+        self.assertEqual(0, content['ItemCount'])
+        self.assertEqual(key_schema, content['KeySchema'])
+        self.assertEqual(0, content['TableSizeBytes'])
+
+        expected = provisioned_throughput.copy()
+        expected['NumberOfDecreasesToday'] = 0
+        for key, value in expected.items():
+            self.assertIn(key, content['ProvisionedThroughput'])
+            self.assertIsInstance(value, int)
+
+        if local_secondary_indexes is None:
+            self.assertNotIn('LocalSecondaryIndexes', content)
+        else:
+            expected = local_secondary_indexes.copy()
+            expected['IndexSizeBytes'] = 0
+            expected['IndexStatus'] = 'CREATING'
+            expected['ItemCount'] = 0
+            self.assertEqual(expected, content['LocalSecondaryIndexes'])
+
+        if global_secondary_indexes is None:
+            self.assertNotIn('GlobalSecondaryIndexes', content)
+        else:
+            expected = global_secondary_indexes.copy()
+            # TODO(yyekovenko): Check all attributes of GSI
+            self.assertEqual(expected, content['GlobalSecondaryIndexes'])
+
+        # CreationDateTime
+        # TODO(yyekovenko): 0 returned for now. Find a way to get exp. time
+        self.assertIn('CreationDateTime', content)
+
+        # TODO(yyekovenko): Later should be changed to just "CREATING" (async)
+        self.assertIn(content['TableStatus'], ['CREATING', 'ACTIVE'])
+
+    def test_duplicate_table(self):
+        tname = rand_name().replace('-', '')
+        self.client.create_table(self.smoke_attrs, tname,
+                                 self.smoke_schema,
+                                 self.smoke_throughput)
+        self.assertTrue(self.wait_for_table_active(tname))
+        self.addResourceCleanUp(self.client.delete_table, tname)
+
+        self.assertBotoError(
+            self.errors.client.ResourceInUseException_DuplicateTable,
+            self.client.create_table,
+            self.smoke_attrs, tname,
+            self.smoke_schema, self.smoke_throughput)
+
+        new_tables = [n for n in self.client.list_tables()['TableNames']
+                      if n == tname]
+        self.assertEqual(1, len(new_tables))
+
+    def test_create_table_index_20_non_key_3_indexes(self):
+        tname = rand_name().replace('-', '')
+        index_attrs = [{'AttributeName': 'attr_name' + str(i),
+                        'AttributeType': 'S'} for i in range(1, 4)
+                       ]
+        non_key_attributes = (['non_key_attr' + str(i) for i in range(1, 8)],
+                              ['non_key_attr' + str(i) for i in range(8, 15)],
+                              ['non_key_attr' + str(i) for i in range(15, 21)])
+        request_lsi = []
+        for i, attr_name in enumerate(index_attrs):
+            request_lsi.append(
+                {
+                    'IndexName': '%s_index' % attr_name['AttributeName'],
+                    'KeySchema': [
+                        {'AttributeName': self.hashkey, 'KeyType': 'HASH'},
+                        {'AttributeName': attr_name['AttributeName'],
+                         'KeyType': 'RANGE'}
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'INCLUDE',
+                        'NonKeyAttributes': non_key_attributes[i]}
+                }
+            )
+        body = self.client.create_table(
+            self.smoke_attrs + index_attrs,
+            tname,
+            self.smoke_schema,
+            self.smoke_throughput,
+            request_lsi)
+        self.assertTrue(self.wait_for_table_active(tname))
+        self.addResourceCleanUp(self.client.delete_table, tname)
+        indexes = body['TableDescription']['LocalSecondaryIndexes']
+
+        for i in range(0, 3):
+            self.assertIn('NonKeyAttributes', indexes[i]['Projection'])
+            index_name = indexes[i]['IndexName']
+            non_key_attrs = [
+                lsi['Projection']['NonKeyAttributes']
+                for lsi in request_lsi if lsi['IndexName'] == index_name
+            ][0]
+            self.assertEqual(len(non_key_attrs), len(indexes[i]['Projection']
+                                                     ['NonKeyAttributes']))
---
+++ contrib/tempest/tempest/api/keyvalue/stable/rest/test_list_table.py
@@ -0,0 +1,178 @@
+# Copyright 2014 Symantec Corporation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import time
+
+from tempest.api.keyvalue.rest_base.base import MagnetoDBTestCase
+from tempest.common.utils.data_utils import rand_name
+from tempest import exceptions
+from tempest.test import attr
+
+
+class MagnetoDBListTableTestCase(MagnetoDBTestCase):
+
+    force_tenant_isolation = True
+
+    def setUp(self):
+        super(MagnetoDBListTableTestCase, self).setUp()
+        self.tables = []
+
+    def tearDown(self):
+        for tname in self.tables:
+            try:
+                self.client.delete_table(tname)
+            except (exceptions.BadRequest, exceptions.NotFound):
+                self.tables.remove(tname)
+        while True:
+            for tname in self.tables:
+                try:
+                    self.client.describe_table(tname)
+                except exceptions.NotFound:
+                    self.tables.remove(tname)
+            if not self.tables:
+                break
+            time.sleep(1)
+        super(MagnetoDBListTableTestCase, self).tearDown()
+
+    @attr(type=['LisT-1'])
+    def test_list_tables_empty(self):
+        headers, body = self.client.list_tables()
+        expected = {'tables': []}
+        self.assertEqual(body, expected)
+
+    @attr(type=['LisT-2'])
+    def test_list_tables(self):
+        tname = rand_name().replace('-', '')
+        self._create_test_table(self.smoke_attrs, tname,
+                                self.smoke_schema,
+                                wait_for_active=True,
+                                cleanup=False)
+        self.tables.append(tname)
+        headers, body = self.client.list_tables()
+        url = self.client.base_url
+        expected = {'tables': [{'href': '{url}/tables/{table}'.format(
+            url=url, table=tname), 'rel': 'self'}]}
+        self.assertEqual(body, expected)
+
+    def _create_n_tables(self, num):
+        for i in range(0, num):
+            tname = rand_name().replace('-', '')
+            self._create_test_table(self.smoke_attrs, tname,
+                                    self.smoke_schema,
+                                    cleanup=False,
+                                    wait_for_active=True)
+            self.tables.append(tname)
+
+    @attr(type=['LisT-10'])
+    def test_list_tables_no_limit_5_tables(self):
+        tnum = 5
+        self._create_n_tables(tnum)
+        headers, body = self.client.list_tables()
+        self.assertEqual(len(body['tables']), tnum)
+
+    @attr(type=['LisT-12'])
+    def test_list_tables_limit_3_10_tables_with_exclusive(self):
+        tnum = 10
+        limit = 3
+        self._create_n_tables(tnum)
+        last_evaluated_table_name = None
+        url = self.client.base_url
+        for i in range(0, tnum / limit):
+            headers, body = self.client.list_tables(
+                limit=limit,
+                exclusive_start_table_name=last_evaluated_table_name)
+            last_evaluated_table_name = body['last_evaluated_table_name']
+            self.assertEqual(len(body['tables']), limit)
+            self.assertEqual(body['tables'][-1]['href'],
+                             '{url}/tables/{table}'.format(
+                             url=url, table=last_evaluated_table_name))
+        headers, body = self.client.list_tables(
+            limit=limit,
+            exclusive_start_table_name=last_evaluated_table_name)
+        self.assertEqual(len(body['tables']), tnum % limit)
+
+    @attr(type=['LisT-30'])
+    def test_list_tables_no_exclusive(self):
+        tnum = 5
+        self._create_n_tables(tnum)
+        headers, body = self.client.list_tables()
+        self.assertEqual(len(body['tables']), tnum)
+
+    @attr(type=['LisT-31', 'LisT-33'])
+    def test_list_tables_exclusive(self):
+        tnum = 5
+        limit = 3
+        self._create_n_tables(tnum)
+        headers, body = self.client.list_tables(limit=limit)
+        last_evaluated_table_name = body['last_evaluated_table_name']
+        headers, body = self.client.list_tables(
+            limit=limit,
+            exclusive_start_table_name=last_evaluated_table_name)
+        self.assertEqual(len(body['tables']), tnum % limit)
+
+    @attr(type=['LisT-32'])
+    def test_list_tables_exclusive_no_previous_run(self):
+        tnum = 5
+        self._create_n_tables(tnum)
+        self.tables.sort()
+        last_evaluated_table_name = self.tables[0]
+        headers, body = self.client.list_tables(
+            exclusive_start_table_name=last_evaluated_table_name)
+        self.assertEqual(len(body['tables']), tnum - 1)
+
+    @attr(type=['LisT-34'])
+    def test_list_tables_exclusive_3_symb(self):
+        tnames = 'aaa', 'bbb'
+        for tname in tnames:
+            self._create_test_table(self.smoke_attrs, tname,
+                                    self.smoke_schema,
+                                    cleanup=False,
+                                    wait_for_active=True)
+            self.tables.append(tname)
+        last_evaluated_table_name = self.tables[0]
+        headers, body = self.client.list_tables(
+            exclusive_start_table_name=last_evaluated_table_name)
+        self.assertEqual(len(body['tables']), 1)
+
+    @attr(type=['LisT-38'])
+    def test_list_tables_exclusive_non_existent(self):
+        tnames = 'aaa1', 'bbb'
+        for tname in tnames:
+            self._create_test_table(self.smoke_attrs, tname,
+                                    self.smoke_schema,
+                                    cleanup=False,
+                                    wait_for_active=True)
+            self.tables.append(tname)
+        last_evaluated_table_name = 'aaa'
+        headers, body = self.client.list_tables(
+            exclusive_start_table_name=last_evaluated_table_name)
+        self.assertEqual(len(body['tables']), 2)
+
+    @attr(type=['LisT-16', 'negative'])
+    def test_list_tables_limit_negative2(self):
+        with self.assertRaises(exceptions.BadRequest) as raises_cm:
+            self.client.list_tables(limit=-2)
+        error_msg = raises_cm.exception._error_string
+        self.assertIn("Bad Request", error_msg)
+        self.assertIn("'limit' property value[-2] is less then min_value",
+                      error_msg)
+
+    @attr(type=['LisT-17', 'negative'])
+    def test_list_tables_limit_string(self):
+        with self.assertRaises(exceptions.BadRequest) as raises_cm:
+            self.client.list_tables(limit="str")
+        error_msg = raises_cm.exception._error_string
+        self.assertIn("Bad Request", error_msg)
+        self.assertIn("Integer is expected", error_msg)
