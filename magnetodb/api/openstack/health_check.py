418736335f4fbcc122e843f19584cce8f54e2ed3
Oleksandr Minakov <alexander_minakov@symantec.com>
2014-12-30 16:53:54 +0200

    Adds healthcheck of RabbitMQ
    Change-Id: Ia24b2359da6a425c7e40db1c7ea4b852f1bd5254
    Implements: bp rabbitmq-healthcheck

--- magnetodb/api/openstack/health_check.py
+++ magnetodb/api/openstack/health_check.py
@@ -15,7 +15,7 @@
 
 """
 Health check request is a lightweight request that allows to check availability
-of magnetodb-api service and its subsystems (Keystone, DB back-end).
+of magnetodb-api service and its subsystems (Keystone, DB back-end, MQ).
 """
 
 import urlparse
@@ -24,8 +24,10 @@ from keystoneclient.generic import client
 
 from magnetodb.api import with_global_env
 from magnetodb.common import exception
+from magnetodb.common import config
 from magnetodb import storage
 from magnetodb.openstack.common.log import logging
+from kombu import Connection
 
 LOG = logging.getLogger(__name__)
 
@@ -33,6 +35,7 @@ STATUS_OK = '200'
 STATUS_ERROR = '503'
 
 keystoneclient = client.Client()
+CONF = config.CONF
 
 
 class HealthCheckApp(object):
@@ -71,6 +74,7 @@ class HealthCheckApp(object):
     def _check_subsystems(self):
         cas_error_msg = 'Cassandra: ERROR'
         key_error_msg = 'Keystone: ERROR'
+        que_error_msg = 'RabbitMQ: ERROR'
         resp = ''
         try:
             storage.health_check()
@@ -82,7 +86,39 @@ class HealthCheckApp(object):
                 resp = '. '.join([resp, key_error_msg])
             else:
                 resp = key_error_msg
+
+        try:
+            self._check_queue()
+        except Exception:
+            if resp:
+                resp = '. '.join([resp, que_error_msg])
+            else:
+                resp = que_error_msg
+
         return resp
+
+    def _check_queue(self):
+        url = (
+            'amqp://{}:{}@{}:{}//'
+        ).format(
+            CONF.rabbit_userid,
+            CONF.rabbit_password,
+            CONF.rabbit_host,
+            CONF.rabbit_port
+        )
+        with Connection(url) as conn:
+            simple_queue = conn.SimpleQueue('simple_queue')
+            message = 'Healthcheck sent'
+            simple_queue.put(message)
+            LOG.debug('Sent: %s' % message)
+            simple_queue.close()
+
+        with Connection(url) as conn:
+            simple_queue = conn.SimpleQueue('simple_queue')
+            message = simple_queue.get(block=True, timeout=1)
+            LOG.debug("Received: %s" % message.payload)
+            message.ack()
+            simple_queue.close()
 
 
 @with_global_env(default_program='magnetodb-api')
