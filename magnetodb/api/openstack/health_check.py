18291b6f8d97c241bd2ecb3176482b615ee94871
Andrei V. Ostapenko <Andrey_Ostapenko@symantec.com>
2015-01-02 18:45:49 +0200

    Adds detailed response body to success healthcheck request
    Implements: bp healthcheck-detailed
    Change-Id: I69668fd7ff13a178dfa926c502d83f672e1b2819

--- magnetodb/api/openstack/health_check.py
+++ magnetodb/api/openstack/health_check.py
@@ -31,19 +31,55 @@ from magnetodb.openstack.common.log import logging
 
 LOG = logging.getLogger(__name__)
 
-STATUS_OK = '200'
-STATUS_ERROR = '503'
-
 keystoneclient = client.Client()
 CONF = config.CONF
 
 
+def check_identity(auth_uri=''):
+    if not keystoneclient.discover(auth_uri):
+        raise exception.MagnetoError("Keystone discover failure")
+
+def check_storage():
+    storage.health_check()
+
+def check_queue():
+    url = (
+        'amqp://{}:{}@{}:{}//'
+    ).format(
+        CONF.rabbit_userid,
+        CONF.rabbit_password,
+        CONF.rabbit_host,
+        CONF.rabbit_port
+    )
+    with kombu.Connection(url) as conn:
+        simple_queue = conn.SimpleQueue('simple_queue')
+        message = 'Healthcheck sent'
+        simple_queue.put(message)
+        LOG.debug('Sent: %s' % message)
+
+        message = simple_queue.get(block=True, timeout=1)
+        LOG.debug("Received: %s" % message.payload)
+        message.ack()
+        simple_queue.close()
+
+
 class HealthCheckApp(object):
     """ Controller for health check request. """
+    STATUS_OK = '200'
+    STATUS_ERROR = '503'
+    error_msg = 'ERROR'
+    success_msg = 'OK'
+
+    default_exception = Exception
 
     def __init__(self, auth_uri=''):
         super(HealthCheckApp, self).__init__()
-        self.auth_uri = auth_uri
+        self.subsystem_check_tuple = (
+            ("Identity", check_identity, (auth_uri,), exception.MagnetoError),
+            ("Storage", check_storage, (),
+             exception.BackendInteractionException),
+            ("Messaging", check_queue, (), self.default_exception),
+        )
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
@@ -54,68 +90,42 @@ class HealthCheckApp(object):
             start_response('404 Not found', [('Content-Type', 'text/plain')])
             return 'Incorrect url. Please check it and try again\n'
 
-        resp = ''
+        resp = 'API: ' + self.success_msg
+        error_status = self.STATUS_OK
+
         query_string = environ.get('QUERY_STRING', '')
         fullcheck = urlparse.parse_qs(query_string).get('fullcheck')
+        subsystems_status = {}
+        if (isinstance(fullcheck, list) and ('true' in fullcheck or
+                'True' in fullcheck)):
+            subsystems_status = self._check_subsystems()
 
-        if isinstance(fullcheck, list) and 'true' in fullcheck:
-            resp = self._check_subsystems()
-
-        if not resp:
-            error_status = STATUS_OK
-            resp = 'OK'
-        else:
-            error_status = STATUS_ERROR
+        if subsystems_status:
+            resp = ', '.join([resp, self._format_resp(subsystems_status)])
+        if self.error_msg in subsystems_status.values():
+            error_status = self.STATUS_ERROR
 
         resp += '\n'
         start_response(error_status, [('Content-Type', 'text/plain')])
         return resp
 
+    def _format_resp(self, body):
+        messages = []
+        for k, v in body.iteritems():
+            messages.append(": ".join([k, v]))
+        return ", ".join(messages)
+
     def _check_subsystems(self):
-        cas_error_msg = 'Cassandra: ERROR'
-        key_error_msg = 'Keystone: ERROR'
-        que_error_msg = 'RabbitMQ: ERROR'
-        resp = ''
-        try:
-            storage.health_check()
-        except exception.BackendInteractionException:
-            resp = cas_error_msg
-
-        if not keystoneclient.discover(self.auth_uri):
-            if resp:
-                resp = '. '.join([resp, key_error_msg])
+        results = {}
+        for subsystem, check, args, ex in self.subsystem_check_tuple:
+            try:
+                check(*args)
+            except ex:
+                results[subsystem] = self.error_msg
             else:
-                resp = key_error_msg
+                results[subsystem] = self.success_msg
 
-        try:
-            self._check_queue()
-        except Exception:
-            if resp:
-                resp = '. '.join([resp, que_error_msg])
-            else:
-                resp = que_error_msg
-
-        return resp
-
-    def _check_queue(self):
-        url = (
-            'amqp://{}:{}@{}:{}//'
-        ).format(
-            CONF.rabbit_userid,
-            CONF.rabbit_password,
-            CONF.rabbit_host,
-            CONF.rabbit_port
-        )
-        with kombu.Connection(url) as conn:
-            simple_queue = conn.SimpleQueue('simple_queue')
-            message = 'Healthcheck sent'
-            simple_queue.put(message)
-            LOG.debug('Sent: %s' % message)
-
-            message = simple_queue.get(block=True, timeout=1)
-            LOG.debug("Received: %s" % message.payload)
-            message.ack()
-            simple_queue.close()
+        return results
 
 
 @with_global_env(default_program='magnetodb-api')
