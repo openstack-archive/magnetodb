759e048a7f02715158c3501b267c4b18b0a0f3a5
Andrei V. Ostapenko <Andrey_Ostapenko@symantec.com>
2015-01-08 16:56:55 +0200

    Fixes tempest plugin due to tempest update
    Also fixes bug with rabbitmq failures
    
    Closes-bug: #1408664
    Closes-bug: #1408901
    Change-Id: I067bcfce30c5ec0c371b5d5b81651719e0f594a2

--- magnetodb/notifier/__init__.py
+++ magnetodb/notifier/__init__.py
@@ -26,7 +26,7 @@ from magnetodb.openstack.common import context as ctxt
 
 extra_notifier_opts = [
     cfg.StrOpt('notification_service',
-               default=mdb_common.PROJECT_NAME,
+               default='magnetodb_notify',
                help='Service publisher_id for outgoing notifications'),
     cfg.StrOpt('default_publisher_id',
                default=None,
