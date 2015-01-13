2dda3d1529ab7882ed454d6e057ed70e7bb4fcbe
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2015-01-13 17:31:18 +0200

    Add restore manager
    Implement restore job metainfo repository
    
    Implements blueprint: restore-job-repo
    
    Change-Id: I32a3a5d95515559e9dd89173904d7649e9b828b8

--- contrib/tempest/tempest/api/keyvalue/stable/rest/test_list_restores.py
+++ contrib/tempest/tempest/api/keyvalue/stable/rest/test_list_restores.py
@@ -13,6 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import uuid
+
 from tempest.api.keyvalue.rest_base.base import MagnetoDBTestCase
 from tempest.common.utils.data_utils import rand_name
 
@@ -28,5 +30,35 @@ class MagnetoDBListRestoresTest(MagnetoDBTestCase):
                                 self.smoke_schema,
                                 wait_for_active=True)
 
-        headers, body = self.management_client.list_restore_jobs(self.tname)
-        self.assertEqual([], body['restore_jobs'])
+        buuid1 = uuid.uuid4()
+        self.management_client.create_restore_job(
+            self.tname, buuid1.hex)
+
+        buuid2 = uuid.uuid4()
+        self.management_client.create_restore_job(
+            self.tname, buuid2.hex)
+
+        buuid3 = uuid.uuid4()
+        self.management_client.create_restore_job(
+            self.tname, buuid3.hex)
+
+        headers, body = self.management_client.list_restore_jobs(
+            self.tname, limit=2)
+        self.assertEqual(2, len(body['restore_jobs']))
+
+        last_eval_id = body['last_evaluated_restore_job_id']
+        restore_jobs1 = body['restore_jobs']
+
+        headers, body = self.management_client.list_restore_jobs(
+            self.tname, exclusive_start_restore_job_id=last_eval_id, limit=2)
+        self.assertEqual(1, len(body['restore_jobs']))
+
+        restore_jobs2 = body['restore_jobs']
+
+        buuids = [b['backup_id'] for b in restore_jobs1]
+        buuids.extend([b['backup_id'] for b in restore_jobs2])
+
+        self.assertEqual(3, len(buuids))
+        self.assertTrue(buuid1.hex in buuids)
+        self.assertTrue(buuid2.hex in buuids)
+        self.assertTrue(buuid3.hex in buuids)
---
+++ magnetodb/storage/restore_info_repo/__init__.py
@@ -0,0 +1,81 @@
+# Copyright 2015 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from magnetodb.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class RestoreInfoRepository(object):
+
+    def get(self, context, table_name, restore_job_id):
+        """Get restore job info
+
+        :param context: current request context
+        :param table_name: string, name of the restored table
+        :param restore_job_id: string, id of the restore job
+
+        :returns: RestoreJobMeta
+
+        :raises: BackendInteractionException
+        """
+        raise NotImplementedError()
+
+    def list(self, context, table_name,
+             exclusive_start_restore_job_id=None,
+             limit=None):
+        """List restore job info items for a table
+
+        :param context: current request context
+        :param table_name: string, name of the restored table
+        :param exclusive_start_restore_job_id: string, last restore job id,
+                retrieved in previous list call
+        :param limit:int, limit of returned restore job info items
+
+        :returns: list of RestoreJobMeta
+
+        :raises: BackendInteractionException
+        """
+        raise NotImplementedError()
+
+    def save(self, context, restore_job_meta):
+        """Save restore job info
+
+        :param context: current request context
+        :param restore_job_meta: RestoreJobMeta, restore job info to save
+
+        :returns: RestoreJobMeta
+
+        :raises: BackendInteractionException
+        """
+        raise NotImplementedError()
+
+    def update(self, context, table_name, restore_job_id,
+               status=None, finish_date_time=None):
+        """Update editable attributes of restore job info
+
+        :param context: current request context
+        :param table_name: string, name of the restored table
+        :param restore_job_id: string, id of the restore job
+        :param status: string, new value for status
+                or None to keep it as is
+        :param finish_date_time: datetime, new value for finish_date_time
+                or None to keep it as is
+
+        :returns: updated RestoreJobMeta
+
+        :raises: BackendInteractionException
+        """
+        raise NotImplementedError()
