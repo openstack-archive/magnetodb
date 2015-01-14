6477b082665605379e07ee5b5f7cd84982b49b8a
Oleksandr Minakov <alexander_minakov@symantec.com>
2015-01-14 15:57:57 +0200

    Adds ability to delete tables from error state
    Adds workflow for deleting tables from CREATE_FAILED
    and DELETE_FAILED states.
    
    Change-Id: Ie93db94bf6317c158c8fe69b57859a41c9c657d6
    Closes-bug: #1407566

--- magnetodb/storage/table_info_repo/__init__.py
+++ magnetodb/storage/table_info_repo/__init__.py
@@ -12,6 +12,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+from magnetodb.storage import models
 
 
 class TableInfo(object):
@@ -24,6 +25,13 @@ class TableInfo(object):
         self.internal_name = internal_name
         self.creation_date_time = creation_date_time
         self.last_update_date_time = last_update_date_time
+
+    @property
+    def in_use(self):
+        return (
+            self.status == models.TableMeta.TABLE_STATUS_CREATING or
+            self.status == models.TableMeta.TABLE_STATUS_DELETING
+        )
 
     @property
     def name(self):
