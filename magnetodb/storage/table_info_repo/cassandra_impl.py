7e4fc1cfbd4e203b171d6e1e695a47b9aa4fe705
dukhlov <d.ukhlov@gmail.com>
2015-01-23 12:24:34 -0500

    Migrate custom local secondary indices to Cassandra 2.1.2
    Implements: bp migrate-cassandra-custom-lsi-driver, bp update-cassandra-version
    
    Change-Id: I659821f2f638865d957f707fac4d01b57c8875a5

--- magnetodb/storage/table_info_repo/cassandra_impl.py
+++ magnetodb/storage/table_info_repo/cassandra_impl.py
@@ -159,11 +159,9 @@ class CassandraTableInfoRepository(table_info_repo.TableInfoRepository):
 
     def __refresh(self, context, table_info, field_list=__field_list):
         query_builder = collections.deque()
-        query_builder.append("SELECT ")
-        query_builder.append(",".join(map('"{}"'.format, field_list)))
 
         query_builder.append(
-            " FROM {} WHERE tenant='{}' AND name='{}'".format(
+            "SELECT * FROM {} WHERE tenant='{}' AND name='{}'".format(
                 self.SYSTEM_TABLE_TABLE_INFO,
                 context.tenant, table_info.name
             )
@@ -179,6 +177,8 @@ class CassandraTableInfoRepository(table_info_repo.TableInfoRepository):
                 "Table '{}' does not exist".format(table_info.name)
             )
         for name, value in result[0].iteritems():
+            if name not in self.__field_list:
+                continue
             if name == "schema":
                 value = models.ModelBase.from_json(value)
             setattr(table_info, name, value)
@@ -286,6 +286,13 @@ class CassandraTableInfoRepository(table_info_repo.TableInfoRepository):
     def delete(self, context, table_name):
         query = (
             "DELETE FROM {}"
+            " WHERE tenant='{}' AND name='{}'".format(
+                self.SYSTEM_TABLE_TABLE_INFO, context.tenant, table_name
+            )
+        )
+        self.__cluster_handler.execute_query(query, consistent=True)
+        query = (
+            "SELECT * FROM {}"
             " WHERE tenant='{}' AND name='{}'".format(
                 self.SYSTEM_TABLE_TABLE_INFO, context.tenant, table_name
             )
