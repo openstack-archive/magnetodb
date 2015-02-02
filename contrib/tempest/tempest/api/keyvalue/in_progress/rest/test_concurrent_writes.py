d085544375b973af7fb2f6b5ff46c24a1174a261
Andrei V. Ostapenko <Andrey_Ostapenko@symantec.com>
2015-02-02 20:48:51 +0200

    Adds concurrent tests
    Adds test for checking atomicity of update item with "ALL_OLD" return
    values with existing fields to update.
    Adds test for checking concurrent writes of different existing fields.
    
    Partially implements: bp test-concurrent-writes
    Change-Id: I310ba24f1ac141b23cf488517221f9d9cb3ebfaf

--- contrib/tempest/tempest/api/keyvalue/in_progress/rest/test_concurrent_writes.py
+++ contrib/tempest/tempest/api/keyvalue/in_progress/rest/test_concurrent_writes.py
@@ -28,7 +28,7 @@ class MagnetoDBConcurrentUpdateTestCase(rest_base.MagnetoDBConcurrentTestCase):
         return ''.join(random.choice(string.lowercase + string.digits)
                        for i in range(length))
 
-    def test_concurrent_writes_200_fields(self):
+    def test_concurrent_writes_200_new_fields(self):
         self.table_name = rand_name(self.table_prefix).replace('-', '')
         self._create_test_table(
             [{'attribute_name': 'hash_attr', 'attribute_type': 'S'},
@@ -52,7 +52,7 @@ class MagnetoDBConcurrentUpdateTestCase(rest_base.MagnetoDBConcurrentTestCase):
                 "extra_attr" + str(i): {
                     "action": "PUT",
                     "value": {
-                        "S": "extra_value" + str(i)
+                        "S": "value" + str(i)
                     }
                 }
             } for i in xrange(0, updates_count)
@@ -85,8 +85,11 @@ class MagnetoDBConcurrentUpdateTestCase(rest_base.MagnetoDBConcurrentTestCase):
             expected_item.update({k: v})
         self.assertEqual(expected_item, body["item"])
 
-    def test_concurrent_writes_10_fields_all_old_non_existent_fiels(self):
-        """Testing atomicity of update item with return values ALL_OLD"""
+    def test_concurrent_writes_10_fields_all_old_new_fiels(self):
+        """Testing atomicity of update item with return values ALL_OLD
+        
+        Updating new fields.
+        """
 
         self.table_name = rand_name(self.table_prefix).replace('-', '')
         self._create_test_table(
@@ -105,26 +108,18 @@ class MagnetoDBConcurrentUpdateTestCase(rest_base.MagnetoDBConcurrentTestCase):
                 "S": "range_value",
             }
         }
-        # this allows to work around bug #1416112
-        workaround_item = {
-            "workaround_field": {
-                "S": "workaround_value",
-            }
-        }
-        workaround_item.update(key)
-
         updates_count = 10
         attribute_updates_list = [
             {
                 "extra_attr" + str(i): {
                     "action": "PUT",
                     "value": {
-                        "S": "extra_value" + str(i)
+                        "S": "value" + str(i)
                     }
                 }
             } for i in xrange(0, updates_count)
         ]
-        self.client.put_item(self.table_name, workaround_item)
+        self.client.put_item(self.table_name, key)
 
         done_count = [0]
         done_event = threading.Event()
@@ -155,3 +150,141 @@ class MagnetoDBConcurrentUpdateTestCase(rest_base.MagnetoDBConcurrentTestCase):
         ])
         # if operation is atomical all results will be unique
         self.assertEqual(len(unique_results), len(results))
+
+    def test_concurrent_writes_10_fields_all_old_existing_fiels(self):
+        """Testing atomicity of update item with return values ALL_OLD
+        
+        Updating existing fields.
+        """
+
+        self.table_name = rand_name(self.table_prefix).replace('-', '')
+        self._create_test_table(
+            [{'attribute_name': 'hash_attr', 'attribute_type': 'S'},
+             {'attribute_name': 'range_attr', 'attribute_type': 'S'}],
+            self.table_name,
+            [{'attribute_name': 'hash_attr', 'key_type': 'HASH'},
+             {'attribute_name': 'range_attr', 'key_type': 'RANGE'}],
+            wait_for_active=True)
+
+        key = {
+            "hash_attr": {
+                "S": "hash_value",
+            },
+            "range_attr": {
+                "S": "range_value",
+            }
+        }
+        updates_count = 10
+        item = {
+            "extra_attr" + str(i): {"S": "old_value" + str(i)}
+                for i in xrange(0, updates_count)
+        }
+        item.update(key)
+        self.client.put_item(self.table_name, item)
+
+        attribute_updates_list = [
+            {
+                "extra_attr" + str(i): {
+                    "action": "PUT",
+                    "value": {
+                        "S": "new_value" + str(i)
+                    }
+                }
+            } for i in xrange(0, updates_count)
+        ]
+
+        done_count = [0]
+        done_event = threading.Event()
+        results = []
+
+        def callback(future):
+            try:
+                headers, body = future.result()
+                results.append(body)
+            finally:
+                done_count[0] += 1
+                if done_count[0] >= updates_count:
+                    done_event.set()
+
+        for attribute_updates in attribute_updates_list:
+            future = self._async_request(
+                'update_item',
+                self.table_name,
+                key,
+                attribute_updates=attribute_updates,
+                return_values="ALL_OLD"
+            )
+            future.add_done_callback(callback)
+        done_event.wait()
+
+        unique_results = set([
+            json.dumps(item['attributes'], sort_keys=True) for item in results
+        ])
+        # if operation is atomical all results will be unique
+        self.assertEqual(len(unique_results), len(results))
+
+    def test_concurrent_writes_200_existing_fields(self):
+        self.table_name = rand_name(self.table_prefix).replace('-', '')
+        self._create_test_table(
+            [{'attribute_name': 'hash_attr', 'attribute_type': 'S'},
+             {'attribute_name': 'range_attr', 'attribute_type': 'S'}],
+            self.table_name,
+            [{'attribute_name': 'hash_attr', 'key_type': 'HASH'},
+             {'attribute_name': 'range_attr', 'key_type': 'RANGE'}],
+            wait_for_active=True)
+
+        key = {
+            "hash_attr": {
+                "S": "hash_value",
+            },
+            "range_attr": {
+                "S": "range_value",
+            }
+        }
+        updates_count = 200
+
+        item = {
+            "extra_attr" + str(i): {"S": "old_value" + str(i)}
+                for i in xrange(0, updates_count)
+        }
+        item.update(key)
+        self.client.put_item(self.table_name, item)
+
+        attribute_updates_list = [
+            {
+                "extra_attr" + str(i): {
+                    "action": "PUT",
+                    "value": {
+                        "S": "new_value" + str(i)
+                    }
+                }
+            } for i in xrange(0, updates_count)
+        ]
+
+        done_count = [0]
+        done_event = threading.Event()
+
+        def callback(future):
+            try:
+                future.result()
+            finally:
+                done_count[0] += 1
+                if done_count[0] >= updates_count:
+                    done_event.set()
+
+        for attribute_updates in attribute_updates_list:
+            future = self._async_request(
+                'update_item',
+                self.table_name,
+                key,
+                attribute_updates=attribute_updates
+            )
+            future.add_done_callback(callback)
+        done_event.wait()
+        headers, body = self.client.get_item(self.table_name, key)
+        expected_item = key
+        for attribute_updates in attribute_updates_list:
+            k = attribute_updates.keys()[0]
+            v = attribute_updates[k]["value"]
+            expected_item.update({k: v})
+        self.assertEqual(expected_item, body["item"])
